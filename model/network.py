fa28461b936216d940296e6db55bbd70ffd9666c
steffen_wedig <steffen.wedig@tum.de>
2023-07-06 12:13:04 +0200

    Created files for nn

---
+++ model/network.py
@@ -0,0 +1,39 @@
+
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        ##############
+        ##############
+
+        self.model = nn.Sequential(
+            nn.Linear(2, 40),
+            nn.Tanh(),
+            nn.Linear(40, 80),
+            nn.Tanh(),
+            nn.Linear(80, 100),
+            nn.Tanh(),
+            nn.Linear(100, 100),
+            nn.Tanh(),
+            nn.Linear(100, 100),
+            nn.Tanh(),
+            nn.Linear(100, 80),
+            nn.Tanh(),
+            nn.Linear(80, 40),
+            nn.Tanh(),
+            nn.Linear(40, 3)
+        )
+
+        ##############
+        ##############
+
+    def forward(self, x):
+        ##############
+        ##############
+
+        x = self.model(x)
+
+        ##############
+        ##############
+        return x
